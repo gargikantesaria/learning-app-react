15992b9e6161ca4d93752233eacf0cb08e25c482
gargikantesaria@gmail.com <gargikantesaria@gmail.com>
2021-03-04 16:36:29 +0530

    [fix] loader

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const loader = document.querySelector('#loader');
 
 // if you want to show the loader when React loads data again
-const showLoader = () => loader.classList.remove('loader--hide');
+const showLoader = () => loader.classList.remove('hideLoader');
 
-const hideLoader = () => loader.classList.add('loader--hide');
+const hideLoader = () => loader.classList.add('hideLoader');
 
 setTimeout(() => {
   ReactDOM.render(
